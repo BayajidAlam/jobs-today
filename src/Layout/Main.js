29564dba22435e97364a9e8d8c74b6ac92ab0c29
bayajidalam <bayajidalam2001@gmail.com>
2023-02-16 20:24:17 +0600

    nav addec

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
-import NavBar from '../pages/Shared/Footer/NavBar';
+import NavBar from '../pages/Shared/NavBar/NavBar';
+
+
 
 const Main = () => {
   return (
