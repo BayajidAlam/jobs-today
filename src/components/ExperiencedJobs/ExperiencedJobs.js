720ae583110fc93413d95895daea0cde146ff810
bayajidalam <bayajidalam2001@gmail.com>
2023-02-17 12:56:20 +0600

    Footer added

--- src/components/ExperiencedJobs/ExperiencedJobs.js
+++ src/components/ExperiencedJobs/ExperiencedJobs.js
@@ -15,7 +15,7 @@ const ExperiencedJobs = () => {
       <p className="text-center">Job Categories</p>
       <div className="container mx-auto">
         <h1 className="text-center lg:text-3xl text-2xl font-bold mb-4">
-          Fresher Jobs
+        Experienced Jobs
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
